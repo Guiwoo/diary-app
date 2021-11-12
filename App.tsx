754d2df7a25b6df45305c25a6aa2755c63929697
Guiwoo <park.guiwoo@hotmail.com>
2021-11-12 14:27:18 +0900

    realm setup & apploading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,37 @@
+import React, { useState } from "react";
+import Realm from "realm";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import AppLoading from "expo-app-loading";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primarKey: "_id",
+};
+
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: "guiwooDiaryDB",
+      schema: [FeelingSchema],
+    });
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={(e) => console.log(e)}
+        startAsync={startLoading}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <NavigationContainer>
       <Navigator />
